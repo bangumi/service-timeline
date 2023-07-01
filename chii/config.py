ba4b96c8875a6311b2dab559991426df5c2237bf
Trim21 <trim21.me@gmail.com>
2023-07-01 09:50:22 +0800

    fix

--- chii/config.py
+++ chii/config.py
@@ -2,7 +2,8 @@ import uuid
 from typing import Optional
 
 from dotenv import load_dotenv
-from pydantic import AnyHttpUrl, BaseSettings, Field
+from pydantic import AnyHttpUrl, Field
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
