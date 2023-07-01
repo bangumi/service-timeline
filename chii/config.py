48371c6d956e35058372248daf530e263d7353c4
Trim21 <trim21.me@gmail.com>
2023-07-01 10:00:26 +0800

    fix

--- chii/config.py
+++ chii/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     # 微服务相关的环境变量
     node_id: str = Field(str(uuid.uuid4()), env="NODE_ID")
     etcd_prefix: str = Field("/chii/services", env="ETCD_PREFIX")
-    etcd_addr: Optional[AnyHttpUrl] = Field(env="ETCD_ADDR")
+    etcd_addr: Optional[AnyHttpUrl] = Field(None, env="ETCD_ADDR")
     external_address: str = Field("127.0.0.1", env="EXTERNAL_ADDRESS")
 
     MYSQL_HOST: str = Field(env="MYSQL_HOST", default="127.0.0.1")
@@ -39,7 +39,7 @@ class Settings(BaseSettings):
 
 
 load_dotenv()
-config = Settings()  # type: ignore
+config = Settings()
 
 if __name__ == "__main__":
     print(config.model_dump())
